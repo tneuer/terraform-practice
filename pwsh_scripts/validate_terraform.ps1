10ddcd537019131e794df0ebdf5d36d61ea7a068
tneuer <thomas.neuer@uzh.ch>
2023-12-12 20:32:30 +0100

    Added better tests

---
+++ pwsh_scripts/validate_terraform.ps1
@@ -0,0 +1,10 @@
+az login;
+$KEYVAULT_NAME='tfstatekv';
+$SECRET_NAME='tfstatekv';
+$env:ARM_ACCESS_KEY=$(az keyvault secret show --name $SECRET_NAME --vault-name $KEYVAULT_NAME --query value -o tsv);
+
+terraform.exe init -backend-config="./configs/config_dev.azure.tfbackend";
+terraform.exe fmt -recursive;
+terraform.exe validate;
+terraform.exe plan -var-file="./variables/dev.tfvars";
+az logout;

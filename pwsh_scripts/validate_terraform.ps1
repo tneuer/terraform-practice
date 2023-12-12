fa38c3d3c849be38965aa977d60c035f07f7b839
tneuer <thomas.neuer@uzh.ch>
2023-12-12 21:18:37 +0100

    Try validation

--- pwsh_scripts/validate_terraform.ps1
+++ pwsh_scripts/validate_terraform.ps1
@@ -1,4 +1,3 @@
-az login;
 $KEYVAULT_NAME='tfstatekv';
 $SECRET_NAME='tfstatekv';
 $env:ARM_ACCESS_KEY=$(az keyvault secret show --name $SECRET_NAME --vault-name $KEYVAULT_NAME --query value -o tsv);
@@ -7,4 +6,3 @@ terraform.exe init -backend-config="./configs/config_dev.azure.tfbackend";
 terraform.exe fmt -recursive;
 terraform.exe validate;
 terraform.exe plan -var-file="./variables/dev.tfvars";
-az logout;

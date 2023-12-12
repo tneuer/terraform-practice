f2173640a71d740dd4be16adade1b5eb7bb9260b
tneuer <thomas.neuer@uzh.ch>
2023-12-12 21:32:06 +0100

    ts

--- pwsh_scripts/validate_terraform.ps1
+++ pwsh_scripts/validate_terraform.ps1
@@ -2,7 +2,7 @@ $KEYVAULT_NAME='tfstatekv';
 $SECRET_NAME='tfstatekv';
 $env:ARM_ACCESS_KEY=$(az keyvault secret show --name $SECRET_NAME --vault-name $KEYVAULT_NAME --query value -o tsv);
 
-terraform.exe init -backend-config="./configs/config_dev.azure.tfbackend";
-terraform.exe fmt -recursive;
-terraform.exe validate;
-terraform.exe plan -var-file="./variables/dev.tfvars";
+terraform init -backend-config="./configs/config_dev.azure.tfbackend";
+terraform fmt -recursive;
+terraform validate;
+terraform plan -var-file="./variables/dev.tfvars";

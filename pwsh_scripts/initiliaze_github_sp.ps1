b2c68143fe197dc280467605e89ab78415c199ed
tneuer <thomas.neuer@uzh.ch>
2023-12-12 21:52:46 +0100

    test

--- pwsh_scripts/initiliaze_github_sp.ps1
+++ pwsh_scripts/initiliaze_github_sp.ps1
@@ -1,3 +1,7 @@
 $SUBSCRIPTION_ID=$(az account show --query id --output tsv);
 $RESOURCE_GROUP_NAME='tf_statefile_rg';
-az ad sp create-for-rbac --name "TerraformDevOpsGithubAction" --role contributor --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME --sdk-auth
+$KEYVAULT_NAME='tfstatekv';
+az ad sp create-for-rbac --name "TerraformDevOpsGithubAction" --role contributor --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME --sdk-auth;
+
+$APP_ID=$(az ad sp list --display-name Terraform --query "[].{spID:appId}" --output tsv);
+az keyvault set-policy --name $KEYVAULT_NAME --spn $APP_ID --secret-permissions get

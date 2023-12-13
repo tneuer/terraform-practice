5d57973731aba042d120d4c26a334a95c3113163
tneuer <thomas.neuer@uzh.ch>
2023-12-13 09:09:31 +0100

    Added better json credentials

--- pwsh_scripts/initialize_github_sp.ps1
+++ pwsh_scripts/initialize_github_sp.ps1
@@ -6,7 +6,13 @@ $APP_ID=$(az ad sp list --display-name $GITHUB_TF_SP_NAME --query "[].{spID:appI
 az role assignment create --assignee-object-id  $APP_ID --role contributor --subscription $SUBSCRIPTION_ID --assignee-principal-type ServicePrincipal --scope /subscriptions/$SUBSCRIPTION_ID/resourcegroups/$BUILD_RESOURCE_GROUP_NAME_RESOURCES
 az keyvault set-policy --name $TF_KEYVAULT_NAME --spn $APP_ID --secret-permissions get
 
+az ad app federated-credential create --id  $APP_ID --parameters ./pwsh_scripts/github_oicd_credential.json
+
 Write-Host Setting Github secrets... -ForegroundColor blue
-gh secret set AZURE_CLIENT_ID --body "$APP_ID" --repos tneuer/terraform-practice;
-gh secret set AZURE_TENANT_ID --body "$TENANT_ID" --repos tneuer/terraform-practice;
-gh secret set AZURE_SUBSCRIPTION_ID --body "$SUBSCRIPTION_ID" --repos tneuer/terraform-practice;
+Write-Host AZURE_CLIENT_ID=$APP_ID
+Write-Host AZURE_SUBSCRIPTION_ID=$SUBSCRIPTION_ID
+Write-Host AZURE_TENANT_ID=$TENANT_ID
+gh auth login
+gh secret set AZURE_CLIENT_ID --body ${APP_ID} --repos tneuer/terraform-practice;
+gh secret set AZURE_TENANT_ID --body ${TENANT_ID} --repos tneuer/terraform-practice;
+gh secret set AZURE_SUBSCRIPTION_ID --body ${SUBSCRIPTION_ID} --repos tneuer/terraform-practice;

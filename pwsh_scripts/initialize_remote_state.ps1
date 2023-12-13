891ae6cc4fc1c1b5c99a016a8e769180f7c7ebf0
tneuer <thomas.neuer@uzh.ch>
2023-12-13 14:55:23 +0100

    Added destroyer

--- pwsh_scripts/initialize_remote_state.ps1
+++ pwsh_scripts/initialize_remote_state.ps1
@@ -10,4 +10,3 @@ az storage container create --name $TF_CONTAINER_NAME --account-name $TF_STORAGE
 Write-Host Deploying keyvault $TF_KEYVAULT_NAME... -ForegroundColor blue
 az keyvault create --name $TF_KEYVAULT_NAME --resource-group $TF_RESOURCE_GROUP_NAME --location $LOCATION
 az keyvault secret set --name $TF_SECRET_NAME --vault-name $TF_KEYVAULT_NAME --value $(az storage account keys list --resource-group $TF_RESOURCE_GROUP_NAME --account-name $TF_STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
-
